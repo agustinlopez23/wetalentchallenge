d4c30a4fc8426766208c5976eed9f13d9f8cb4a9
Your Name <you@example.com>
2023-04-16 06:20:00 -0300

    seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,62 +2,38 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-async function seed () {
-  // Insertar categorías
+async function main () {
   const categories = await prisma.category.createMany({
     data: [
-      { title: 'Noticias' },
-      { title: 'Deportes' },
-      { title: 'Tecnología' },
-      { title: 'Entretenimiento' }
+      { title: 'Database' },
+      { title: 'Programming' },
+      { title: 'Health' },
+      { title: 'Wellness' }
     ]
   })
 
-  // Insertar publicaciones
   const posts = await prisma.post.createMany({
     data: [
       {
-        title: 'Título de la primera publicación',
-        content: 'Contenido de la primera publicación',
-        categories: {
-          connect: [
-            { id: categories[0].id }, // Categoría "Noticias"
-            { id: categories[3].id } // Categoría "Entretenimiento"
-          ]
-        }
+        title: 'Introduction to SQL',
+        content: 'SQL is a powerful database language used to manage data.'
       },
       {
-        title: 'Título de la segunda publicación',
-        content: 'Contenido de la segunda publicación',
-        categories: {
-          connect: [
-            { id: categories[1].id }, // Categoría "Deportes"
-            { id: categories[2].id } // Categoría "Tecnología"
-          ]
-        }
+        title: 'The Benefits of Exercise',
+        content: 'Exercise has been shown to improve overall health and reduce the risk of chronic diseases.'
       },
       {
-        title: 'Título de la tercera publicación',
-        content: 'Contenido de la tercera publicación',
-        categories: {
-          connect: [
-            { id: categories[0].id }, // Categoría "Noticias"
-            { id: categories[1].id }, // Categoría "Deportes"
-            { id: categories[2].id } // Categoría "Tecnología"
-          ]
-        }
+        title: 'Python vs Java',
+        content: 'Python and Java are two popular programming languages used for developing software applications.'
       }
     ]
   })
 
-  console.log('Seed completado')
+  console.log('Data seeded successfully')
 }
 
-seed()
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+main()
+  .catch((e) => console.error(e))
   .finally(async () => {
     await prisma.$disconnect()
   })
